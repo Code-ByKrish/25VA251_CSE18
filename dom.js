b18484596351654e569d61e29f64bba092e94a84
Krish Patel <krish2025f@gmail.com>
2026-04-09 05:00:06 +0530

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,9 @@
+function result() {
+
+    // change text color
+    document.getElementById("text").style.color = "red";
+
+    // change background color
+    document.getElementsByTagName("body")[0].style.backgroundColor = "yellow";
+
+}
